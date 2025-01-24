74ea6e9cc31968986f71925cf0eae116e1f3388f
Andre_Junior <42242398+anndrehjr@users.noreply.github.com>
2025-01-24 14:05:32 -0300

    .

--- src/App.js
+++ src/App.js
@@ -236,7 +236,7 @@ export default function App() {
                       className="w-full h-56 object-cover"
                     />
                     <div className="p-6">
-                      <h2 className="text-2xl font-bold mb-4 text-gray-600 dark:text-gray-900">
+                      <h2 className="text-2xl font-bold mb-4 text-gray-600 dark:text-gray-800">
                         {service.title}
                       </h2>
                       <p className="text-gray-600 dark:text-gray-900 mb-6">
